b1d190e7ad767d14a446f9124e6861b4373bb939
vicent <vicent@myariadna.com>
2026-01-08 17:28:02 +0100

    final dia

--- api/comparativa/comparativa_mysql.js
+++ api/comparativa/comparativa_mysql.js
@@ -7,20 +7,34 @@ const clientes_mysql = {
     },
     datos_comparativa: async (data, empresa) => {
         let conn = undefined;
-        
+        let empresavinculada = 0;
+        let arr = [];
+        let result = [];
         try {
             let cfg = await connector.empresa(empresa)
             conn = await mysql.createConnection(cfg);
 
+            //recuperamos el valor de spara1 empresavinculada
+            let sql = "SELECT empresavinculada from spara1";
+            let [param] = await conn.query(sql);
+            if (param[0].empresavinculada) empresavinculada = param[0].empresavinculada
             // Primero establece el idioma español para los nombres de mes
             await conn.query("SET lc_time_names = 'es_ES'");
 
-            let sql = recuperaSql(data);
-
-            let [result] = await conn.query(sql);
+            if(data.codagent != data.hcodagent){    
+              for(let i=data.codagent; i<=data.hcodagent ; i++){
+                sql = recuperaSql(data, empresavinculada, i);
+                 [result] = await conn.query(sql);
+                 arr.push(...result);
+              }
+            } else {
+               sql = recuperaSql(data, empresavinculada, data.codagent);
+              [result] = await conn.query(sql);
+              arr.push(...result);
+            }
+           
             await conn.end();
-            return result;
-
+            return arr;
         } catch (error) {
             if (conn) {
                 await conn.end();
@@ -31,150 +45,126 @@ const clientes_mysql = {
 
 }
 
-const recuperaSql = function(data) {
-    let sql = '';
-    if(!data.anyoAnterior) {
-        sql = `SELECT 
-              '${data.nomEmpre}' AS nomempre,
-              YEAR(sc.fecfactu) AS anyo,
-              MONTH(sc.fecfactu) AS num_mes, 
-              MONTHNAME(sc.fecfactu) AS nombre_mes, 
-              SUM(COALESCE(sc.baseimp1, 0)) AS total_baseimp1, 
-              SUM(COALESCE(sc.baseimp2, 0)) AS total_baseimp2, 
-              SUM(COALESCE(sc.baseimp3, 0)) AS total_baseimp3, 
-              SUM(COALESCE(sc.baseimp4, 0)) AS total_baseimp4, 
-              SUM(COALESCE(sc.baseimp5, 0)) AS total_baseimp5, 
-              SUM(COALESCE(sc.baseimp1, 0)) + 
-              SUM(COALESCE(sc.baseimp2, 0)) + 
-              SUM(COALESCE(sc.baseimp3, 0)) + 
-              SUM(COALESCE(sc.baseimp4, 0)) + 
-              SUM(COALESCE(sc.baseimp5, 0)) AS total_mes_agente, 
-              tmp.total_mes AS total_mes, 
-              sa.nomagent as nomagent,
-              ROUND(( 
-                (SUM(COALESCE(sc.baseimp1, 0)) + 
-                 SUM(COALESCE(sc.baseimp2, 0)) + 
-                 SUM(COALESCE(sc.baseimp3, 0)) + 
-                 SUM(COALESCE(sc.baseimp4, 0)) + 
-                 SUM(COALESCE(sc.baseimp5, 0))) / NULLIF(tmp.total_mes, 0) 
-              ) * 100, 2) AS porcentaje_agente 
-            FROM scafac AS sc 
-            LEFT JOIN sagent as sa on sa.codagent = sc.codagent
-            LEFT JOIN ( 
-              SELECT 
-                MONTH(sc.fecfactu) AS num_mes, 
-                SUM(COALESCE(sc.baseimp1, 0)) + 
-                SUM(COALESCE(sc.baseimp2, 0)) + 
-                SUM(COALESCE(sc.baseimp3, 0)) + 
-                SUM(COALESCE(sc.baseimp4, 0)) + 
-                SUM(COALESCE(sc.baseimp5, 0)) AS total_mes 
-              FROM scafac AS sc 
-              WHERE sc.fecfactu BETWEEN '${data.dateFormat}' AND  '${data.hDateFormat}'
-              GROUP BY MONTH(sc.fecfactu) 
-            ) AS tmp ON MONTH(sc.fecfactu) = tmp.num_mes 
-            WHERE sc.codagent = ${data.codagent} 
-              AND sc.fecfactu BETWEEN  '${data.dateFormat}' AND '${data.hDateFormat}'  
-            GROUP BY num_mes, nombre_mes, tmp.total_mes 
-            ORDER BY num_mes`
-    } else {
-                        sql = `SELECT
-                        '${data.nomEmpre}' AS nomempre,
-                        m.num_mes,
-                        MONTHNAME(STR_TO_DATE(m.num_mes, '%m')) AS nombre_mes,
+const recuperaSql = (data, empresavinculada, cod) => {
+    return `
+    SELECT
+        '${data.nomEmpre}' AS nomempre,
+        m.num_mes,
+        MONTHNAME(STR_TO_DATE(m.num_mes, '%m')) AS nombre_mes,
 
-                        COALESCE(curr.anyo, YEAR(DATE_SUB('${data.dateFormat}', INTERVAL 0 YEAR))) AS anyo_actual,
-                        COALESCE(curr.total_baseimp1, 0) AS total_baseimp1,
-                        COALESCE(curr.total_baseimp2, 0) AS total_baseimp2,
-                        COALESCE(curr.total_baseimp3, 0) AS total_baseimp3,
-                        COALESCE(curr.total_baseimp4, 0) AS total_baseimp4,
-                        COALESCE(curr.total_baseimp5, 0) AS total_baseimp5,
-                        COALESCE(curr.total_mes_agente, 0) AS total_mes_agente,
+        -- Datos año actual
+        COALESCE(curr.anyo, YEAR('${data.dateFormat}')) AS anyo_actual,
+        COALESCE(curr.total_baseimp1,0) AS total_baseimp1,
+        COALESCE(curr.total_baseimp2,0) AS total_baseimp2,
+        COALESCE(curr.total_baseimp3,0) AS total_baseimp3,
+        COALESCE(curr.total_baseimp4,0) AS total_baseimp4,
+        COALESCE(curr.total_baseimp5,0) AS total_baseimp5,
+        COALESCE(curr.total_mes_agente,0) AS total_mes_agente,
+        COALESCE(curr_total.total_mes,0) AS total_mes,
+        ROUND(
+            CASE 
+                WHEN COALESCE(curr_total.total_mes,0) = 0 THEN 0
+                ELSE COALESCE(curr.total_mes_agente,0) / COALESCE(curr_total.total_mes,0) * 100
+            END,
+            2
+        ) AS porcentaje_agente,
 
-                        COALESCE(curr_total.total_mes, 0) AS total_mes,
-                        ROUND(
-                            COALESCE(curr.total_mes_agente, 0) / NULLIF(COALESCE(curr_total.total_mes, 0), 0) * 100, 2
-                        ) AS porcentaje_agente,
+        -- Datos año anterior
+        COALESCE(prev.anyo_anterior, YEAR('${data.dateFormat}') - 1) AS anyo_anterior,
+        COALESCE(prev.total_baseimp1,0) AS total_baseimp1_anterior,
+        COALESCE(prev.total_baseimp2,0) AS total_baseimp2_anterior,
+        COALESCE(prev.total_baseimp3,0) AS total_baseimp3_anterior,
+        COALESCE(prev.total_baseimp4,0) AS total_baseimp4_anterior,
+        COALESCE(prev.total_baseimp5,0) AS total_baseimp5_anterior,
+        COALESCE(prev.total_mes_agente_anterior,0) AS total_mes_agente_anterior,
+        COALESCE(prev_total.total_mes_anterior,0) AS total_mes_anterior,
+        -- Porcentaje agente año anterior (sobre el total acumulado del mes del año anterior)
+        ROUND(
+            CASE 
+                WHEN COALESCE(prev_total.total_mes_anterior,0) = 0 THEN 0
+                ELSE COALESCE(prev.total_mes_agente_anterior,0) / COALESCE(prev_total.total_mes_anterior,0) * 100
+            END,
+            2
+        ) AS porcentaje_agente_anterior,
 
-                        COALESCE(prev.anyo_anterior, YEAR(DATE_SUB('${data.dateFormat}', INTERVAL 1 YEAR))) AS anyo_anterior,
-                        COALESCE(prev.total_baseimp1, 0) AS total_baseimp1_anterior,
-                        COALESCE(prev.total_baseimp2, 0) AS total_baseimp2_anterior,
-                        COALESCE(prev.total_baseimp3, 0) AS total_baseimp3_anterior,
-                        COALESCE(prev.total_baseimp4, 0) AS total_baseimp4_anterior,
-                        COALESCE(prev.total_baseimp5, 0) AS total_baseimp5_anterior,
-                        COALESCE(prev.total_mes_agente_anterior, 0) AS total_mes_agente_anterior,
-                        
-                        COALESCE(prev_total.total_mes_anterior, 0) AS total_mes_anterior,
-                        COALESCE(
-                            ROUND(
-                            ((curr.total_mes_agente - prev.total_mes_agente_anterior) / NULLIF(prev.total_mes_agente_anterior, 0)) * 100,
-                            2
-                            ),
-                            0
-                        ) AS porcentaje_agente_anterior
+        -- Agente
+        ${cod} AS codagent,
+        (SELECT sa.nomagent FROM sagent sa WHERE sa.codagent = ${cod} LIMIT 1) AS nomagent
 
-                        FROM
-                        (SELECT 1 AS num_mes UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION
-                        SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10 UNION SELECT 11 UNION SELECT 12) AS m
+    FROM
+        -- Lista de meses
+        (SELECT 1 AS num_mes UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION
+         SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10 UNION SELECT 11 UNION SELECT 12) AS m
 
-                        LEFT JOIN
-                        (
-                        SELECT
-                            YEAR(fecfactu) AS anyo,
-                            MONTH(fecfactu) AS num_mes,
-                            SUM(COALESCE(baseimp1,0)) AS total_baseimp1,
-                            SUM(COALESCE(baseimp2,0)) AS total_baseimp2,
-                            SUM(COALESCE(baseimp3,0)) AS total_baseimp3,
-                            SUM(COALESCE(baseimp4,0)) AS total_baseimp4,
-                            SUM(COALESCE(baseimp5,0)) AS total_baseimp5,
-                            SUM(COALESCE(baseimp1,0)) + SUM(COALESCE(baseimp2,0)) + SUM(COALESCE(baseimp3,0)) +
-                            SUM(COALESCE(baseimp4,0)) + SUM(COALESCE(baseimp5,0)) AS total_mes_agente
-                        FROM scafac
-                        WHERE codagent = ${data.codagent}
-                            AND fecfactu BETWEEN '${data.dateFormat}' AND '${data.hDateFormat}'
-                        GROUP BY YEAR(fecfactu), MONTH(fecfactu)
-                        ) AS curr ON curr.num_mes = m.num_mes
+    -- Totales año actual por agente
+    LEFT JOIN (
+        SELECT
+            MONTH(fecfactu) AS num_mes,
+            YEAR(fecfactu) AS anyo,
+            SUM(COALESCE(baseimp1,0)) AS total_baseimp1,
+            SUM(COALESCE(baseimp2,0)) AS total_baseimp2,
+            SUM(COALESCE(baseimp3,0)) AS total_baseimp3,
+            SUM(COALESCE(baseimp4,0)) AS total_baseimp4,
+            SUM(COALESCE(baseimp5,0)) AS total_baseimp5,
+            SUM(
+                COALESCE(baseimp1,0)+COALESCE(baseimp2,0)+COALESCE(baseimp3,0)+COALESCE(baseimp4,0)+COALESCE(baseimp5,0)
+            ) AS total_mes_agente
+        FROM scafac
+        WHERE codagent = ${cod}
+          AND fecfactu BETWEEN '${data.dateFormat}' AND '${data.hDateFormat}'
+        GROUP BY MONTH(fecfactu), YEAR(fecfactu)
+    ) AS curr ON curr.num_mes = m.num_mes
 
-                        LEFT JOIN
-                        (
-                        SELECT
-                            MONTH(fecfactu) AS num_mes,
-                            SUM(COALESCE(baseimp1,0)) + SUM(COALESCE(baseimp2,0)) + SUM(COALESCE(baseimp3,0)) +
-                            SUM(COALESCE(baseimp4,0)) + SUM(COALESCE(baseimp5,0)) AS total_mes
-                        FROM scafac
-                        WHERE fecfactu BETWEEN '${data.dateFormat}' AND '${data.hDateFormat}'
-                        GROUP BY MONTH(fecfactu)
-                        ) AS curr_total ON curr_total.num_mes = m.num_mes
+    -- Totales generales año actual
+    LEFT JOIN (
+        SELECT
+            MONTH(fecfactu) AS num_mes,
+            SUM(
+                COALESCE(baseimp1,0)+COALESCE(baseimp2,0)+COALESCE(baseimp3,0)+COALESCE(baseimp4,0)+COALESCE(baseimp5,0)
+            ) AS total_mes
+        FROM scafac
+        WHERE fecfactu BETWEEN '${data.dateFormat}' AND '${data.hDateFormat}'
+        GROUP BY MONTH(fecfactu)
+    ) AS curr_total ON curr_total.num_mes = m.num_mes
 
-                        LEFT JOIN
-                        (
-                        SELECT
-                            YEAR(fecfactu) AS anyo_anterior,
-                            MONTH(fecfactu) AS num_mes,
-                            SUM(COALESCE(baseimp1,0)) AS total_baseimp1,
-                            SUM(COALESCE(baseimp2,0)) AS total_baseimp2,
-                            SUM(COALESCE(baseimp3,0)) AS total_baseimp3,
-                            SUM(COALESCE(baseimp4,0)) AS total_baseimp4,
-                            SUM(COALESCE(baseimp5,0)) AS total_baseimp5,
-                            SUM(COALESCE(baseimp1,0)) + SUM(COALESCE(baseimp2,0)) + SUM(COALESCE(baseimp3,0)) +
-                            SUM(COALESCE(baseimp4,0)) + SUM(COALESCE(baseimp5,0)) AS total_mes_agente_anterior
-                        FROM scafac
-                        WHERE codagent = ${data.codagent}
-                            AND fecfactu BETWEEN DATE_SUB('${data.dateFormat}', INTERVAL 1 YEAR) AND DATE_SUB('${data.hDateFormat}', INTERVAL 1 YEAR)
-                        GROUP BY YEAR(fecfactu), MONTH(fecfactu)
-                        ) AS prev ON prev.num_mes = m.num_mes
+    -- Totales año anterior por agente
+    LEFT JOIN (
+        SELECT
+            MONTH(fecfactu) AS num_mes,
+            YEAR(fecfactu) AS anyo_anterior,
+            SUM(COALESCE(baseimp1,0)) AS total_baseimp1,
+            SUM(COALESCE(baseimp2,0)) AS total_baseimp2,
+            SUM(COALESCE(baseimp3,0)) AS total_baseimp3,
+            SUM(COALESCE(baseimp4,0)) AS total_baseimp4,
+            SUM(COALESCE(baseimp5,0)) AS total_baseimp5,
+            SUM(
+                COALESCE(baseimp1,0)+COALESCE(baseimp2,0)+COALESCE(baseimp3,0)+COALESCE(baseimp4,0)+COALESCE(baseimp5,0)
+            ) AS total_mes_agente_anterior
+        FROM scafac
+        WHERE codagent = ${cod}
+          AND YEAR(fecfactu) = YEAR('${data.dateFormat}') - 1
+        GROUP BY MONTH(fecfactu), YEAR(fecfactu)
+    ) AS prev ON prev.num_mes = m.num_mes
 
-                        LEFT JOIN
-                        (
-                        SELECT
-                            MONTH(fecfactu) AS num_mes,
-                            SUM(COALESCE(baseimp1,0)) + SUM(COALESCE(baseimp2,0)) + SUM(COALESCE(baseimp3,0)) +
-                            SUM(COALESCE(baseimp4,0)) + SUM(COALESCE(baseimp5,0)) AS total_mes_anterior
-                        FROM scafac
-                        WHERE fecfactu BETWEEN DATE_SUB('${data.dateFormat}', INTERVAL 1 YEAR) AND DATE_SUB('${data.hDateFormat}', INTERVAL 1 YEAR)
-                        GROUP BY MONTH(fecfactu)
-                        ) AS prev_total ON prev_total.num_mes = m.num_mes`;
-    }
-    return sql;
+    -- Totales generales año anterior
+    LEFT JOIN (
+        SELECT
+            MONTH(fecfactu) AS num_mes,
+            SUM(
+                COALESCE(baseimp1,0)+COALESCE(baseimp2,0)+COALESCE(baseimp3,0)+COALESCE(baseimp4,0)+COALESCE(baseimp5,0)
+            ) AS total_mes_anterior
+        FROM scafac
+        WHERE YEAR(fecfactu) = YEAR('${data.dateFormat}') - 1
+        GROUP BY MONTH(fecfactu)
+    ) AS prev_total ON prev_total.num_mes = m.num_mes
+
+    ORDER BY m.num_mes;
+    `;
 }
 
-module.exports = clientes_mysql
+
+
+
+
+
+ module.exports = clientes_mysql
